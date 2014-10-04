457e77c45f86f588bf9abe2cefad6312ddc43fb8
lovexurongquan@163.com <angelrose0>
2014-10-04 17:17:33 +0800

    modefy web

--- Skpic.Sazs.Web/Global.asax
+++ Skpic.Sazs.Web/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Skpic.Sazs.Web.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Skpic.Sazs.Web.WebApiApplication" Language="C#" %>
--- Skpic.Sazs.Web/Global.asax.cs
+++ Skpic.Sazs.Web/Global.asax.cs
@@ -6,23 +6,20 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
 
 namespace Skpic.Sazs.Web
 {
-    // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
-    // 请访问 http://go.microsoft.com/?LinkId=9394801
-
-    public class MvcApplication : System.Web.HttpApplication
+    public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
