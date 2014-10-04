6c86b0aaad298463376e4fb1cad3b503aa1ce089
岳珅 <sazs@live.cn>
2014-10-04 15:06:22 +0800

    仅为上传

---
+++ Skpic.Sazs.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Skpic.Sazs.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
