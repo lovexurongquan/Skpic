7bade90f8969e4c4871353876962f2ffab89fd06
lovexurongquan@163.com <angelrose0>
2014-10-04 17:16:34 +0800

    modefy web

--- Skpic.Sazs.Web/App_Start/AuthConfig.cs
+++ Skpic.Sazs.Web/App_Start/AuthConfig.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Web.WebPages.OAuth;
-
-namespace Skpic.Sazs.Web
+﻿namespace Skpic.Sazs.Web
 {
     public static class AuthConfig
     {
