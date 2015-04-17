8dd1ebb0d433b141d24b873b063201f7292e0b93
Skpic <lovexurongquan@163.com>
2015-04-17 13:45:01 +0800

    add angular other plugins

--- Skpic.Main.Web/App_Start/BundleConfig.cs
+++ Skpic.Main.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Skpic.Main.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                      "~/Scripts/angular.js"));
+                      "~/Scripts/angular/angular.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
